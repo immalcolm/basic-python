3f8f4e88288ae060699493216c3a465b636689aa
Hong Jian Yam <malcolm_yam@np.edu.sg>
2026-02-02 11:17:34 +0800

    Create day1-demo.py with basic Python concepts
    Added a Python script demonstrating basic concepts like BMI calculation, data types, and string manipulation.

---
+++ day1-demo.py
@@ -0,0 +1,43 @@
+print('hihi')
+# tt = float(input("Enter weight kg:")) #65 #integers -> whole numbers
+his is single line comment
+# BMI ? 
+# weight height
+'''
+multi line comment
+'''
+# BMI = weight / (height * height)
+# string is text, we enclose using quote
+# "this is a string"
+# 'this is also a string'
+# basic data types
+height = float(input("Enter height in m")) #float -> decimal values 
+weigh
+bmi = weight/(height*height)
+BMI = weight
+print(BMI)
+# .JPG .JPEG --> 
+# as a developer .. we should rename.. .jpg
+# BMI vs bmi --> they are not the same
+# what are the boundaries?
+# human > 1000kg?
+# height > 3meters ?
+# if (human < 1000kg and height < 3m.... )
+# human > meters? 0? negative?
+
+#str(variable) 
+# str -> function to convert numbers to string values
+print("Your BMI is " + str(bmi))
+# string concatenation 
+quote = "Monday is Bluey" #string variable
+
+print("Today is " + quote) #quote is a string
+quote = "Monday is actually bad... "
+print("Today is " + quote) #quote is a string
+
+#basic data types in python
+a = 10 #integers -> whole numbers
+b = 3.14   #floats -> decimal values
+c = True #booleans -> True or False  | Yes or No | 
+d = "this is a string" #string => text values
+e = 'this is also a string' 
